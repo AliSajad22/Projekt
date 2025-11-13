735a1755210786bbecc9e37cb4f876fe4f0271f7
Ali Sajad Enayat <alisajadenayat2222@gmail.com>
2025-11-13 17:22:29 +0100

    Add files via upload

---
+++ Button/script.js
@@ -0,0 +1,25 @@
+// var btnFirst = document.querySelector(".btn-first");
+// var btnSecond = document.querySelector(".btn-second");
+// var btnThird = document.querySelector(".btn-third");
+// var btnfourth = document.querySelector(".btn-fourth");
+
+// btnFirst.onclick = function () {
+//   console.log("This is the first Button");
+// };
+
+// btnSecond.onclick = function () {
+//   console.log("This is the Second Button");
+// };
+
+// btnThird.onclick = function () {
+//   console.log("This is the Third Button");
+// };
+
+// btnfourth.onclick = function () {
+//   console.log("This is the fourth Button");
+// };
+
+const btn = document.querySelectorAll(".btn");
+btn.forEach.onclick = function () {
+  console.log("This is the first Button");
+};
