4d2483759bae04884006c7a0a8a167ce715142fa
Ali Sajad Enayat <alisajadenayat2222@gmail.com>
2025-11-14 13:34:25 +0100

    Comment out button click event handling
    Comment out button event listeners and related code.

--- Button/script.js
+++ Button/script.js
@@ -1,3 +1,4 @@
+// 1
 // var btnFirst = document.querySelector(".btn-first");
 // var btnSecond = document.querySelector(".btn-second");
 // var btnThird = document.querySelector(".btn-third");
@@ -19,26 +20,47 @@
 //   console.log("This is the fourth Button");
 // };
 
-const buttons = document.querySelectorAll(".btn");
+// 2
+// const buttons = document.querySelectorAll(".btn");
+// console.log(buttons);
 
-buttons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    switch (index) {
-      case 0:
-        console.log("This is the first Button");
-        break;
-      case 1:
-        console.log("This is the second Button");
-        break;
-      case 2:
-        console.log("This is the third Button");
-        break;
-      case 3:
-        console.log("This is the fourth Button");
-        break;
-      default:
-        console.log("Unknown button");
-    }
-  });
-});
+// buttons.forEach((button, index) => {
+//   button.addEventListener("click", () => {
+//     switch (index) {
+//       case 0:
+//         console.log("This is the first Button");
+//         break;
+//       case 1:
+//         console.log("This is the second Button");
+//         break;
+//       case 2:
+//         console.log("This is the third Button");
+//         break;
+//       case 3:
+//         console.log("This is the fourth Button");
+//         break;
+//       default:
+//         console.log("Unknown button");
+//     }
+//   });
+// });
 
+// 3
+
+// const buttons = document.querySelectorAll(".btn");
+
+// buttons[0].onclick = function () {
+//   console.log("This is the first Button");
+// };
+// buttons[1].onclick = function () {
+//   console.log("This is the second Button");
+// };
+// buttons[2].onclick = function () {
+//   console.log("This is the third Button");
+// };
+// buttons[3].onclick = function () {
+//   console.log("This is the fourth Button");
+// };
+// buttons[4].onclick = function () {
+//   console.log("Unknown");
+// };
