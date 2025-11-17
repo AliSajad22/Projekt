f2d2ad71042b9326cd6ac8c3220a3af9fafa95a2
Ali Sajad Enayat <alisajadenayat2222@gmail.com>
2025-11-17 19:54:33 +0100

    Update js.js

--- Neuer Ordner/js.js
+++ Neuer Ordner/js.js
@@ -1,9 +1,32 @@
-const btn = document.getElementById("show__Btn");
-const hiddenItems = document.querySelectorAll(".list__item.hidden");
+// const btn = document.getElementById("show__Btn");
+// const hiddenItems = document.querySelectorAll(".list__item.hidden");
 
-btn.addEventListener("click", function () {
-  hiddenItems.forEach((item) => {
-    item.style.display = "list-item";
-  });
-  btn.style.display = "none";
-});
+// let expanded = false;
+
+// btn.addEventListener("click", () => {
+//   hiddenItems.forEach(item =>
+//     item.style.display = expanded ? "none" : "list-item"
+//   );
+
+//   expanded = !expanded;
+// });
+
+
+// const btn = document.getElementById("show__Btn");
+// const hiddenItems = document.querySelectorAll(".list__item.hidden");
+
+// let expanded = false;
+
+// btn.addEventListener("click", function () {
+//   if (!expanded) {
+//     hiddenItems.forEach((item) => {
+//       item.style.display = "list-item";
+//     });
+//     expanded = true;
+//   } else {
+//     hiddenItems.forEach((item) => {
+//       item.style.display = "none";
+//     });
+//     expanded = false;
+//   }
+// });
